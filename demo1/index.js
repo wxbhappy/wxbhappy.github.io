09a436725fa37b0612e48a48bd867ec1bb400b5a
wu <201257824@qq.com>
2019-07-08 20:02:07 +0800

    demo

--- demo1/index.js
+++
@@ -1,9 +0,0 @@
-$('.item').click(function (e){
-    $(this).parents().find('.wrapper').addClass('wrapper-active');
-    $(this).addClass('active');
-});
-$('.close').click(function (e){
-    e.stopPropagation(); 
-    $(this).parents().find('.item').removeClass('active')
-                                   .end().find('.wrapper-active').removeClass('wrapper-active');
-})
